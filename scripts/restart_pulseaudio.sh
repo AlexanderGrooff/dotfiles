a6b5aa04d1b9c9fab0999b2273a9402a2f8e0972
Alexander Grooff <alexandergrooff@gmail.com>
2020-02-20 15:20:31 +0100

    Restart pulseaudio twice
    Not sure what the cause of this is, but it seems necessary

--- scripts/restart_pulseaudio.sh
+++ scripts/restart_pulseaudio.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-PA_CMD=/usr/bin/pulseaudio
+function fix_pa {
+    PA_CMD=/usr/bin/pulseaudio
 
-# Check if pulseaudio is running
-if $PA_CMD --check; then
-    # Kill the running instance
-    $PA_CMD -k
-fi
+    # Check if pulseaudio is running
+    if $PA_CMD --check; then
+        # Kill the running instance
+        $PA_CMD -k
+    fi
 
-# Restart daemon
-$PA_CMD -D
+    # Restart daemon
+    $PA_CMD -D
+}
+fix_pa
+fix_pa
