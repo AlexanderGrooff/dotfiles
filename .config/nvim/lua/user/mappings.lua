82dab75b06ff6481ccb8fad56f22fe8c89c15844
Alexander Grooff <alexandergrooff@gmail.com>
2023-03-15 11:57:36 +0100

    feat: <C-t> for file finder

--- .config/nvim/lua/user/mappings.lua
+++ .config/nvim/lua/user/mappings.lua
@@ -20,6 +20,7 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<C-t>"] = { function() require("telescope.builtin").find_files() end, desc = "Find files" },
   },
   t = {
     -- setting a mapping to false will disable it
