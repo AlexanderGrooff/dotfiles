2a590a92b6f57a03ba407b062d3f578b49f14835
Alexander Grooff <alexandergrooff@gmail.com>
2023-11-08 08:19:24 +0100

    fix: grep correctly

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -3,13 +3,13 @@
 # Ensure all services are started on boot
 
 # Check if i3 is running
-if [[ ! $(pgrep -x "i3") ]]; then
+if [[ $(pgrep -x "i3") ]]; then
     # Load gnome keyring
     if [[ `command -v gnome-keyring-daemon` ]]; then
         eval $(gnome-keyring-daemon --components=pkcs11,secrets,ssh)
         export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/gcr/ssh
     fi
-elif [[ ${XDG_CURRENT_DESKTOP} = "hyprland" ]]; then
-    systemctl --user start waybar-reload.sevice
+elif [[ ${XDG_CURRENT_DESKTOP} = "Hyprland" ]]; then
+    systemctl --user restart waybar-reload.service
     dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
 fi
