eb67535503847153febfcdd1b99e86b6d825a1c7
Alexander Grooff <alexandergrooff@gmail.com>
2023-11-28 15:38:55 +0100

    feat: use autossh for jumphost

--- bin/jumphost.sh
+++ bin/jumphost.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+# Set up socks proxy over port 5000
 if [[ $(grep "Arch Linux" /etc/os-release) ]]; then
-    ssh jh -N -D 5000
+    eval $(/usr/bin/gnome-keyring-daemon --start)
+    export SSH_AUTH_SOCK
+    autossh -M 0 -D localhost:5000 -N jh
 else
     ssh jh -N -D 5000
 fi
