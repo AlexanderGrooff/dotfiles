63c6ca595cec1b4ad396910021f6c299fdd39e23
Alexander Grooff <alexandergrooff@gmail.com>
2023-11-28 16:07:07 +0100

    feat: load gnome ssh keys

--- bin/jumphost.sh
+++ bin/jumphost.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-# Set up socks proxy over port 5000
 if [[ $(grep "Arch Linux" /etc/os-release) ]]; then
-    eval $(/usr/bin/gnome-keyring-daemon --start)
+    if [ $(command -v gnome-keyring-daemon) ]; then
+        eval $(/usr/bin/gnome-keyring-daemon --start)
+    fi
     export SSH_AUTH_SOCK
     autossh -M 0 -D localhost:5000 -N jh
 else
