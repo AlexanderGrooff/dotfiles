4dbe8796a10f8b6ed46f172243822fa447326d45
Alexander Grooff <alexandergrooff@gmail.com>
2024-05-22 16:22:15 +0200

    feat: manage nvim config in separate repo

--- .config/nvim/lua/community.lua
+++
@@ -1,5 +0,0 @@
-return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.recipes.vscode" },  -- https://github.com/vscode-neovim/vscode-neovim
-  { import = "astrocommunity.pack.lua" },
-}
